18e7441ff16374e047198bf764b0c2478016014a
med-bt <mohamedbt111@gmail.com>
2024-10-12 17:10:09 +0100

    test modified

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import rsa
 
 r = rsa.RSA(2048)
 
-a = r.encrytpt("hello world my name is mohamed this is a secret message ")
+a = r.encrytpt("hello world ! this is a secret message ")
 b = r.decrypt(a)
 print(a)
 print(b)
